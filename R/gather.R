b329fe5f911a25a9f4e9834c3f64ab6ca2286448
Stuart Lee <lee.s@wehi.edu.au>
2019-01-29 15:09:24 +1100

    gather coverage

---
+++ R/gather.R
@@ -0,0 +1,32 @@
+#' A wrapper to compute coverage but only on 'standard' chromosomes
+standardise_coverage <- function(x, genome_info) {
+  plyranges::compute_coverage(x) %>%
+    plyranges::filter_by_overlaps(genome_info)
+}
+
+standardise_reference <- function(x, genome_info) {
+  x %>%
+    plyranges::get_genome_info() %>% 
+    plyranges::filter_by_overlaps(genome_info)
+}
+
+#' Generate long-form coverage as coverage
+#' @param bam
+gather_coverage <- function(bams, genome_info, BPARAM = BiocParallel::bpparam()) {
+  
+  bfl <- lapply(bams,
+                function(.f) Rsamtools::BamFile(.f)
+                ) %>% 
+    Rsamtools::BamFileList()
+  
+  genome_info <- standardise_reference(bfl, genome_info)
+  
+  cvg  <- BiocParallel::bplapply(
+    bfl,
+    FUN = function(x) standardise_coverage(x, genome_info = genome_info),
+    BPARAM = BPARAM
+  ) %>%
+    GenomicRanges::GRangesList()
+  
+  unlist(cvg, use.names = FALSE)
+}
