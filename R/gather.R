83603be6f6ec069756d0451816aa38b497be7398
Stuart Lee <lee.s@wehi.edu.au>
2019-01-31 11:01:26 +1100

    refactor coverage computation

--- R/gather.R
+++ R/gather.R
@@ -1,32 +1,56 @@
 
-#' Generate long-form coverage as coverage
-#' @param bams a character vector containing a list of paths to indexed BAM files
-#' @param genome_info a GRanges object containing reference annotation
-#' @param BPPARAM a BiocParallel object (default = `bpparam()`)
+#' Compute long-form coverage
+#' 
+#' 
+#' @param bams a character vector containing a paths to indexed BAM files
+#' @param genome_info a GRanges object containing reference annotation (default = NULL)
+#' @param BPPARAM a BiocParallel object (default = `BiocParallel::bpparam()`)
+#' 
+#' @details This function computes coverage as a GRanges object, with
+#' a `source` column containing the name of the input BAM file, and a
+#' `score` column containing the coverage score. The `genome_info` argument
+#' takes a GRanges object containing contig/chromosome information. If
+#' supplied coverage will be computed only over those contigs, and the
+#' annotation information will be set on the resulting GRanges to that availble
+#' in `genome_info`.
+#' 
+#' @return a GRanges object
 #' 
 #' @importFrom BiocParallel bpparam bplapply
 #' @importFrom Rsamtools BamFile BamFileList
+#' @importFrom GenomeInfoDb seqinfo seqinfo<- keepSeqlevels
 #' @export
 gather_coverage <- function(bams, genome_info = NULL, BPPARAM = BiocParallel::bpparam()) {
   
   bfl <- Rsamtools::BamFileList(bams)
 
+  sb <- Rsamtools::ScanBamParam()
+  
+  if (!is.null(genome_info)) {
+    sb <- Rsamtools::ScanBamParam(which = genome_info)
+  }
+  
   cvg  <- BiocParallel::bplapply(
     bfl,
-    FUN = compute_coverage,
+    FUN = function(x) {
+        compute_coverage(x, param = sb)
+      },
     BPPARAM = BPPARAM
     ) 
   
   cvg <- GenomicRanges::GRangesList(cvg)
   
-  md <- DataFrame(source =  Rle(names(cvg), lengths(cvg)))
-  
-
-  cvg <- unlist(cvg, use.names = FALSE)
-  mcols(cvg) <- md
-  
   if (!is.null(genome_info)) {
-    return(cvg %>% filter_by_overlaps(genome_info))
+    cvg <- GenomeInfoDb::keepSeqlevels(cvg, 
+                                       GenomeInfoDb::seqnames(genome_info), 
+                                       "tidy")
+    seqinfo(cvg) <- seqinfo(genome_info)
   }
+  
+  md <- S4Vectors::DataFrame(source =  Rle(names(cvg), lengths(cvg)))
+  
+  cvg <- unlist(cvg, use.names = FALSE)
+  mcols(cvg) <- cbind(md, mcols(cvg))
+  
   cvg
 }
