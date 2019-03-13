655190c5d54233ec0ef90071cda4a9fb4b284b02
Stuart Lee <lee.s@wehi.edu.au>
2019-03-13 18:29:14 +1100

    refactoring spread

--- R/detect.R
+++ R/detect.R
@@ -59,18 +59,30 @@ squish_coverage <- function(x, ..., threshold = 0L) {
 #' for each exon/intron variable. 
 #'
 #'@export
-spread_coverage <- function(x, ..., threshold  = 0L) {
-  groups <- set_grouping(...)
-  cols <- .signal_cols(threshold = threshold)
-  res <- x %>% 
-    plyranges::group_by(!!!groups) %>% 
-    plyranges::summarise(
-      !!!cols
-    ) %>% 
-    S4Vectors::transform(
-      prop_bases_covered_at_threshold = n_bases_above_threshold / total_coverage
-    )
+spread_coverage <- function(x, ...) {
+  res <- plyranges::group_by(x, !!!set_grouping_by_gene(...)) 
+  res <- plyranges::summarise(res, !!!.signal_cols())
+  res <- plyranges::as_granges(res, strand = feature_strand)
+  # because of how as_granges dispactches on DataFrame
+  res <- plyranges::mutate(res,
+                           score = IRanges::IntegerList(score),
+                           n_bases = IRanges::IntegerList(n_bases)
+                           )
+  # compute average over all columns
+  res <- plyranges::group_by(res, !!!set_grouping_by_feature(...))
+  res <- plyranges::mutate(res, 
+                           E = mean(average_coverage)
+                           )
+  res <- plyranges::ungroup(res)
+  res <- plyranges::group_by(res, ...)
+  res <- plyranges::mutate(res, 
+                           total_bases_above_E = bases_above_score(score, n_bases, E))
+  return(res)
+  # on remaining columns compute average
   
+  
+    
+
   # spread on feature_type
   res <- S4Vectors::split(res, res[["feature_type"]])
   values <- BiocGenerics::do.call(S4Vectors::cbind, 
@@ -80,28 +92,49 @@ spread_coverage <- function(x, ..., threshold  = 0L) {
 }
 
 # set default groupings
-set_grouping <- function(...) {
+set_grouping_by_gene <- function(...) {
   # on dots
   dots <- rlang::enquos(...)
   # splice with default groups
   rlang::quos(
     !!!dots,
+    seqnames,
     gene_id,
     feature_strand,
     feature_type
   ) 
 }
 
+set_grouping_by_feature <- function(...) {
+  # on dots
+  dots <- rlang::enquos(...)
+  # splice with default groups
+  rlang::quos(
+    !!!dots,
+    feature_type
+  ) 
+}
+
 # weighted coverage computation 
-mean_coverage <- function(score, width) sum(score*width) / sum(width)
+mean_coverage <- function(score, width) {
+  score <- IRanges::NumericList(score)
+  width <- IRanges::NumericList(width)
+  sum(score*width) / sum(width)
+}
+
+bases_above_score <- function(score, width, value) {
+  sum(width[score > value])
+}
 
 # default columns to compute in summary calculations
 .signal_cols <- function(threshold = 0L) {
   rlang::quos(
+    start = min(start),
+    end = max(end),
     feature_length = sum(unique(feature_length)),
-    total_coverage = sum(score * width),
     average_coverage = mean_coverage(score, width),
-    n_bases_above_threshold = sum(width[score > !!threshold]), 
+    score = score,
+    n_bases = width
   )
 }
 
