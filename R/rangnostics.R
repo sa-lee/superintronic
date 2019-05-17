21f4499584e45dd0932f17ef5ba2fa17c4591d59
Stuart Lee <lee.s@wehi.edu.au>
2019-05-17 11:53:46 +1000

    rleviews test

---
+++ R/rangnostics.R
@@ -0,0 +1,33 @@
+#' Compute diagnoistics over Ranges (rangnostics)
+#' 
+#' @param ranges a GRanges object 
+#' @param .var A column in `ranges` defining an index
+#' @param .funs A list of functions to summarise
+#' @param ... additional parameters to pass to ...
+#' 
+#' @export
+setGeneric("rangnostics", function(ranges, .var, .funs, ...) {
+  standardGeneric("rangnostics")
+})
+
+setMethod("rangnostics", "GenomicRanges", 
+          function(ranges, .var, .funs, ...) {
+            sub <- plyranges::select(ranges, !!.var)
+            gr_l <- split(sub, seqnames(sub), drop = TRUE)
+            # split by seqnames
+            rle_list <- as(lapply(gr_l, 
+                                 function(.y) {
+                                   S4Vectors::Rle(mcols(.y)[[.var]], 
+                                       lengths = width(.y))
+                                 }),
+                             "List")
+            ranges_list <- reduce(ranges(gr_l))
+            
+            views_list <- RleViewsList(rleList = rle_list, 
+                                      rangesList = ranges_list)
+
+            lapply(.funs,
+                   function(.f) IRanges::viewApply(views_list, .f)
+                   )
+            
+          })
