1aaf67faad0ad028d960be899456098c966adcd2
Stuart Lee <lee.s@wehi.edu.au>
2019-05-24 15:03:29 +1000

    vignette draf

--- R/rangenostics.R
+++ R/rangenostics.R
@@ -1,34 +1,52 @@
-#' Compute diagnoistics over Ranges (rangenostics)
+#' Compute diagnoistics over Ranges (rango)
 #' 
-#' @param ranges a GRanges object 
-#' @param .var A column in `ranges` defining an index
+#' @param x a Ranges object 
+#' @param .var A meta data column in `x` to summarise over 
 #' @param .funs A list of functions to summarise
-#' @param ... additional parameters to pass to ...
+#' @param ... additional parameters to pass to `.funs`
+#' 
+#' 
+#' @importClassesFrom plyranges GroupedGenomicRanges GroupedIntegerRanges
+#' @importClassesFrom IRanges Ranges
 #' 
 #' @export
-setGeneric("rangenostics", function(ranges, .var, .funs, ...) {
-  standardGeneric("rangenostics")
+setGeneric("rango", function(x, .var, .funs, ...) {
+  standardGeneric("rango")
 })
 
-setMethod("rangenostics", "GenomicRanges", 
-          function(ranges, .var, .funs, ...) {
-            sub <- plyranges::select(ranges, !!.var)
-            gr_l <- split(sub, seqnames(sub), drop = TRUE)
-            # split by seqnames
-            rle_list <- as(lapply(gr_l, 
-                                 function(.y) {
-                                   S4Vectors::Rle(mcols(.y)[[.var]], 
-                                                  lengths = width(.y))
-                                 }),
-                             "List")
-            ranges_list <- reduce(ranges(gr_l), 
-                                  ignore.strand = TRUE)
-            
-            views_list <- RleViewsList(rleList = rle_list, 
-                                       rangesList = ranges_list)
+make_rle <- function(x, .var) {
+  S4Vectors::Rle(mcols(x)[[.var]], lengths = BiocGenerics::width(x))
+} 
 
-            lapply(.funs,
-                   function(.f) IRanges::viewApply(views_list, .f)
-                   )
+make_views <- function(x, .var) {
+  # rle-ise the grouping
+  rle_list <- as(lapply(x, make_rle, .var = .var), "List")
+  
+  # for each group we merge the overlapping ranges 
+  ranges_list <- reduce(IRanges::ranges(x), ignore.strand = TRUE)
+  
+  IRanges::RleViewsList(rleList = rle_list, rangesList = ranges_list)
+  
+}
+
+setMethod("rango", "Ranges", 
+          function(x, .var, .funs, ...) {
+            .var <- rlang::ensym(.var)
+            sub <- plyranges::select(x, !!.var)
+            message("No grouping variable given, using seqnames instead.")
+            x <- split_ranges(x, seqnames)
+            views <- make_views(x, as.character(.var))
+            lapply(.funs, function(.f) IRanges::viewApply(views, .f, ...))
             
           })
+
+
+setMethod("rango", "GroupedGenomicRanges", 
+          function(x, .var, .funs, ...) {
+            .var <- rlang::ensym(.var)
+            sub <- plyranges::select(x, !!.var)
+            x <- split_ranges(x)
+            views <- make_views(x, as.character(.var))
+            lapply(.funs, function(.f) IRanges::viewApply(views, .f, ...))
+  
+})
