65fd84e9bacead988ab6841b1cdf785421b58064
Stuart Lee <lee.s@wehi.edu.au>
2019-05-27 11:00:51 +1000

    rango is now somewhat like summarise at

--- R/rangenostics.R
+++ R/rangenostics.R
@@ -14,19 +14,14 @@ setGeneric("rango", function(x, .var, .funs, ...) {
   standardGeneric("rango")
 })
 
-make_rle <- function(x, .var) {
-  S4Vectors::Rle(mcols(x)[[.var]], lengths = BiocGenerics::width(x))
+make_views <- function(x, .var) {
+  subject <- S4Vectors::Rle(mcols(x)[[.var]], lengths = BiocGenerics::width(x))
+  rng <- reduce(ranges(subject))
+  IRanges::Views(subject, start = rng)
 } 
 
-make_views <- function(x, .var) {
-  # rle-ise the grouping
-  rle_list <- as(lapply(x, make_rle, .var = .var), "List")
-  
-  # for each group we merge the overlapping ranges 
-  ranges_list <- reduce(IRanges::ranges(x), ignore.strand = TRUE)
-  
-  IRanges::RleViewsList(rleList = rle_list, rangesList = ranges_list)
-  
+make_views_list <- function(x, .var) {
+  as(lapply(x, make_views, .var = .var), "List")
 }
 
 setMethod("rango", "Ranges", 
@@ -35,7 +30,7 @@ setMethod("rango", "Ranges",
             sub <- plyranges::select(x, !!.var)
             message("No grouping variable given, using seqnames instead.")
             x <- split_ranges(x, seqnames)
-            views <- make_views(x, as.character(.var))
+            views <- make_views_list(x, as.character(.var))
             lapply(.funs, function(.f) IRanges::viewApply(views, .f, ...))
             
           })
@@ -46,7 +41,7 @@ setMethod("rango", "GroupedGenomicRanges",
             .var <- rlang::ensym(.var)
             sub <- plyranges::select(x, !!.var)
             x <- split_ranges(x)
-            views <- make_views(x, as.character(.var))
+            views <- make_views_list(x, as.character(.var))
             lapply(.funs, function(.f) IRanges::viewApply(views, .f, ...))
   
 })
