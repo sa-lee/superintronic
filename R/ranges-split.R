c3f69f4f0b8ed7e0b9f777e35f256467744b4983
Stuart Lee <lee.s@wehi.edu.au>
2019-06-03 10:46:42 +1000

    tests for split

--- R/ranges-split.R
+++ R/ranges-split.R
@@ -7,6 +7,24 @@
 #' @importClassesFrom plyranges GroupedGenomicRanges GroupedIntegerRanges
 #' @importClassesFrom IRanges Ranges
 #' 
+#' @details If `x` is already grouped, then passing `...` are ignored. If `x`
+#' is not grouped and variable names are provided to `...` these are forwarded
+#' to `[plyranges::group_by()]`, if no variables are provided then x is returned.
+#' 
+#' @examples 
+#' 
+#' lvls <- paste0("chr",c(1:23))
+#' gr <- GRanges(
+#'   seqnames = factor(sample(lvls, 1000, replace = TRUE), levels = lvls),
+#'   ranges = IRanges(start = rpois(1000, 10000), width = rpois(1000, 100)),
+#'   grp = sample(letters[1:4], 1000, replace = TRUE),
+#'   gc = runif(1000)
+#' )
+#' split_ranges(gr, seqnames)
+#' gr_by_grp <- group_by(gr, grp)
+#' split_ranges(gr_by_grp)   
+#' 
+#' 
 #' @export
 #' @rdname split_ranges 
 setGeneric("split_ranges", 
@@ -19,11 +37,18 @@ setGeneric("split_ranges",
 #' @rdname split_ranges
 #' @export
 setMethod("split_ranges", "Ranges",  function(x, ...) {
-  .split_ranges(plyranges::group_by(x, ...))
+  grps <- rlang::enquos(...)
+  if (length(grps) == 0) return(x)
+  
+  .split_ranges(plyranges::group_by(x, !!!grps))
 })
 
 
 .split_ranges <- function(x, ...) {
+  dots <-  rlang::enquos(...)
+  if (length(dots) > 0) {
+    warning(paste("Ignoring arguments in `...`"))
+  }
   grps <- plyranges::groups(x)
   ans <- S4Vectors::split(x@delegate, 
                    plyranges::select(x, !!!grps, .drop_ranges = TRUE),
--- tests/testthat/test-split.R
+++ tests/testthat/test-split.R
@@ -1,3 +1,44 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+context("group by + split")
+
+lvls <- paste0("chr",c(1:23))
+gr <- GRanges(
+  seqnames = factor(sample(lvls, 1000, replace = TRUE), levels = lvls),
+  ranges = IRanges(start = rpois(1000, 10000), width = rpois(1000, 100)),
+  grp = sample(letters[1:4], 1000, replace = TRUE),
+  gc = runif(1000)
+)
+
+groupings <- unique(select(gr, grp, seqnames, .drop_ranges = TRUE))
+
+test_that("returns a GRangesList with desired properties", {
+  # no split just returns
+  expect_identical(split_ranges(gr), gr)
+  
+  # splitting ungrouped produces an unnamed GRangesList
+  ans <- split_ranges(gr, seqnames)
+  expect_true(is(ans, "GRangesList"))
+  expect_equal(length(ans), length(lvls))
+  expect_null(names(ans))
+  
+  # using grouped produces the same answer
+  ans2 <- split_ranges(group_by(gr, seqnames))
+  expect_identical(ans, ans2)
+  expect_true(is(ans2, "GRangesList"))
+  expect_equal(length(ans2), length(lvls))
+  expect_null(names(ans2))
+  
+  # grouping again produces a warning
+  expect_warning(split_ranges(group_by(gr, seqnames), strand))
+  
+  # multiple groups
+  ans <- split_ranges(gr, grp, seqnames)
+  expect_true(is(ans, "GRangesList"))
+  expect_equal(length(ans), nrow(groupings))
+  expect_null(names(ans))
+  
+  ans2 <-split_ranges(group_by(gr, grp, seqnames))
+  expect_identical(ans, ans2)
+  expect_equal(length(ans2), nrow(groupings))
+  expect_null(names(ans2))
+  
 })
