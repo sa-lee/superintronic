145be7188e27044958cbf8041aa2cf55432d17f1
Stuart Lee <lee.s@mac3195.wehi.edu.au>
2019-06-05 18:18:52 +1000

    much simpler api for view

--- R/view-coverage.R
+++ R/view-coverage.R
@@ -1,68 +1,63 @@
-#' View coverage as a stacked area chart
+#' View coverage as an area chart oriented from 5' to 3'
 #' 
-#' @param cvg a GRanges object obtained from `merge_coverage()`
-#' @param target a GRanges object from `prepare_annotation()` containing a gene_id column
-#' @param hline an optional data.frame giving marker points
-#' @param facets a set of variables quoted by `ggplot2::vars()` that is passed 
+#' @param data a GRanges object 
+#' @param score an expression or column in data that represents the coverage score
+#' @param colour a column in the data to highlight ranges by  
+#' @param facets a set of metadata or range variables in `data` quoted by `md_vars()` that is passed 
 #' down to `facet_wrap()`
 #'
-#' 
 #' @importFrom ggplot2 ggplot aes facet_wrap labs theme geom_segment geom_rect element_blank geom_hline
 #' @importFrom patchwork wrap_plots
 #' @export
-view_coverage <- function(cvg, target, hline = NULL, facets = ggplot2::vars()) {
+view_coverage <- function(data, score, colour = NULL, facets = rng_vars()) {
   
-  annotation <- bind_ranges(
-    exon = unlist(target$exonic_parts, use.names = FALSE),
-    intron = unlist(target$intronic_parts, use.names = FALSE),
-    .id = "feature"
-  )
-  
-  annotation_tracks <- view_segments(annotation, color = feature)
-  
-  coverage_view <- plyranges::filter_by_overlaps(cvg, target)
-  coverage_view <- plyranges::mutate(coverage_view, strand = feature_strand)
+  score <- rlang::enquo(score) 
+  colour <- rlang::enquo(colour) 
 
+  no_col <- rlang::quo_is_null(colour)
   if (length(facets) > 0L) {
-    coverage_view <- plyranges::group_by(coverage_view, !!!facets)
+    data <- plyranges::group_by(data, !!!facets)
   } 
   
   # disjoin coverage scores
-  coverage_view <- plyranges::disjoin_ranges_directed(
-    coverage_view,
-    score = mean(log2(score + 1)),
-    feature = unlist(unique(feature_type))
-  ) 
+  if (!no_col) {
+    coverage_view <- plyranges::disjoin_ranges_directed(
+      data,
+      score = mean(!!score),
+      feature = unlist(!!colour)
+    ) 
+  } else {
+    coverage_view <- plyranges::disjoin_ranges_directed(
+      data,
+      score = mean(!!score)
+    ) 
+  }
+
   
   plot_tbl <- as.data.frame(coverage_view)
- 
+  
   cvg_hist <- ggplot(plot_tbl, 
-                     aes(x = start, xend = end, y = 0, yend = score)) +
-    geom_segment(aes(colour = feature)) +
-    rescale_by_width(coverage_view)
+                     aes(x = start, xend = end, y = 0, yend = score))
+    rescale_by_width(data)
   
-  if (!is.null(hline)) {
-    cvg_hist <- cvg_hist + geom_hline(data = hline, aes(yintercept = log2(E +1)))
+  if (!no_col)  {
+    cvg_hist <- cvg_hist + geom_segment(aes(colour = feature))
+  } else {
+    cvg_hist <- cvg_hist + geom_segment()
   }
-  
+    
   if (length(facets) > 0L) {
     cvg_hist <- cvg_hist + facet_wrap(facets, ncol = 1L)
   }
   
   # cosmetic fixes 
   cvg_hist <- cvg_hist +
-    theme_bw() +
     theme(axis.title.x = element_blank(),
           axis.ticks.x = element_blank(),
           axis.text.x = element_blank()) +
-    scale_colour_brewer(palette = "Dark2") +
-    guides(colour = FALSE) +
-    labs(title = paste("Coverage profile:", target$gene_name),
-         y  =  "coverage score (log2)") 
+    labs(y = rlang::quo_get_expr(score))
   
-  patchwork::wrap_plots(cvg_hist, 
-                        annotation_tracks, 
-                        nrow = 2L, 
-                        heights = c(4,1))
+  
+  return(cvg_hist)
 }
 
