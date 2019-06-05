283a85550796bf340f5eed79b6bc3954116cddb6
Stuart Lee <lee.s@wehi.edu.au>
2019-06-05 11:23:39 +1000

    changes to rangle

--- R/rangenostics.R
+++ R/rangenostics.R
@@ -51,11 +51,15 @@ setMethod("rangle", "GenomicRanges",
 #'@export
 setMethod("rangle", "GroupedGenomicRanges", 
           function(x, .var, .index, .funs) {
-            
+            .groups <- plyranges::group_vars(x)
             .index <- rlang::enquo(.index)
+            .index_c <- as.character(rlang::quo_get_expr(.index))
+            .regroups <- union(.groups, .index_c)
             .var <- rlang::enquo(.var)
             .var_c <- as.character(rlang::quo_get_expr(.var))
+            x <- group_by(ungroup(x), !!!rlang::syms(.regroups))
             y <- split_ranges(x)
+            
             views <- make_views_list(y, .var_c)
             # need to check name input for list
             res <- lapply(
@@ -71,13 +75,18 @@ setMethod("rangle", "GroupedGenomicRanges",
             })
             
             res <- S4Vectors::DataFrame(res)
-            return(res)
-            regions <- summarise(x, 
-                                 start = min(start), 
-                                 end = max(end), 
-                                 seqnames =  unique(seqnames))
+            regions <- summarise(x,
+                                 seqnames_x = unlist(BiocGenerics::unique(seqnames)),
+                                 strand_x = unlist(BiocGenerics::unique(strand)),
+                                 start_x = min(start),
+                                 end_x= max(end)
+                                 )
             res <- cbind(regions, res)
-            GenomicRanges::makeGRangesFromDataFrame(res, 
-                                                    keep.extra.columns = TRUE)
+            
+            plyranges::as_granges(res, 
+                                  seqnames = seqnames_x, 
+                                  strand = strand_x,
+                                  start = start_x,
+                                  end = end_x)
             
 })
